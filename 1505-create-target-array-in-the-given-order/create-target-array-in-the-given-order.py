098c227fc4f8bcdd6910253593873e245ef91eef
Mamun Reja <mamun20030318reja@gmail.com>
2026-07-26 20:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.29%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        target = []
+        for num, pos in zip(nums, index):
+            target.insert(pos, num)
+        return target       
